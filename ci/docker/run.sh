d7dc160f6eedacf0a414fb79b7282129ee23d07e
Ariguun Bolorbold <bolorboldariguun16@gmail.com>
2025-10-07 22:55:20 +0300

    chore: :wrench: configure CI

--- ci/docker/run.sh
+++ ci/docker/run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 set -e
-
 cd "$(dirname "$0")"
+
+export MSYS_NO_PATHCONV=1
 
 docker run \
     -it \
     --rm \
-    --volume $(pwd)/../..:/xv6 vityamand/xv6:latest \
+    --volume "$(pwd)/../..:/xv6" vityamand/xv6:latest \
     /bin/bash
